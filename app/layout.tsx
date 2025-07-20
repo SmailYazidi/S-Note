9705d697ac77fd5734414c6731c29f5f7a6d4940
smailyazidi <smailyazidi@gmail.com>
2025-07-20 12:11:16 +0100

    Updated personal info

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 import "./globals.css"
-import { AppProviders } from "./providers"
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -18,7 +17,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
-        <AppProviders>{children}</AppProviders>
+        {children}
       </body>
     </html>
   )
